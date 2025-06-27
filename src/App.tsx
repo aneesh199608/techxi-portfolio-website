a062ad42a74280ebf33bf95db54bf42b249aea65
“Aneesh” <30018808+aneesh199608@users.noreply.github.com>
2025-06-27 13:38:16 +0530

    feat: enable shadcn dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { Button } from "@/components/ui/button"
 import { NavigationMenuDemo } from "./components/sections/Navbar"
+import { ThemeProvider } from "@/components/theme-provider"
 
 function App() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
-      <NavigationMenuDemo />
-    </div>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <div className="flex min-h-svh flex-col items-center justify-center">
+        <Button>Click me</Button>
+        <NavigationMenuDemo />
+      </div>
+    </ThemeProvider>
+    
   )
 }
 
