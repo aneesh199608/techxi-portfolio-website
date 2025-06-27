84d8a3f6c48cf872a56c88e18a0e8364495c5b85
“Aneesh” <30018808+aneesh199608@users.noreply.github.com>
2025-06-27 13:36:11 +0530

    feat: add navbar component example

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Button } from "@/components/ui/button"
+import { NavigationMenuDemo } from "./components/sections/Navbar"
 
 function App() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center">
       <Button>Click me</Button>
+      <NavigationMenuDemo />
     </div>
   )
 }
